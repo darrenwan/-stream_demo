da53141143e2d0b0db425a47b8e152ffe872bb70
darrenwan <darrenwan@126.com>
2022-10-18 13:03:56 +0800

    author update

---
+++ README.md
@@ -0,0 +1,3 @@
+# only test
+
+这个示例使用了: `matplotlib`, `pandas`, `scipy` 和 `Pillow`｡
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"tobyglei@gmail.com\"\n\
+email = \"darrenwanxx@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
